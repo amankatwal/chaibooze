6021000548dc5450a16896a1c0eab7f71a1d9747
amankatwal <118984365+amankatwal@users.noreply.github.com>
2023-01-23 19:04:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const a= document.querySelector(".navbar");
+const b= document.querySelector(".mobile-navbar");
+b.addEventListener('click', function(){
+b.classList.toggle('active');
+a.classList.toggle('active');
+
+}
+)
